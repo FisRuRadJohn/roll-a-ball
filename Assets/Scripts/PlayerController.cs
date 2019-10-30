0de3436e5780e4e674da6ec8dfa60e0538fef5db
pene <lope>
2019-10-30 11:52:34 +0100

    Create a dards-balls videogame

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,15 @@ public class PlayerController : MonoBehaviour
     public Rigidbody myRigiBody;
     public float speed;
 
+
+    int score;
+   // private Vector3 ipo;
+
     // Start is called before the first frame update
     void Start()
     {
+        // ipo = new Vector3(0,0,0);
+        score = 0;
     }
 
     // Update is called once per frame
@@ -25,5 +31,17 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(movementHorizontal, 0f, movementVertical);
         myRigiBody.AddForce(movement * speed);
+    }
+
+    //comparamos mediante el uso de tags para identificar el objeto que recogemos
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pt"))
+        {
+            other.gameObject.SetActive(false);
+
+            score += other.gameObject.GetComponent<Pickup>().points;
+            Debug.Log("puntuacion: " + score);
+        }
     }
 }
